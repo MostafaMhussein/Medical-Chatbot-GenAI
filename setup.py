3a14bbec7a16bf3a5b9cfaf914a404f93a7fe059
MostafaMhussein <mustafa.m.hussien@hotmail.com>
2025-10-17 07:02:56 +0400

    updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Mostafa Morsy',
+    author_email= 'mostafa.m.hussien@hotmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
